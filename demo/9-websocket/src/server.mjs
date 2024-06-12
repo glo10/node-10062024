24411f0d5d960704bbb2471f672f888ae84cbefd
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-06-12 15:17:28 +0200

    feat: demo socket.io

---
+++ demo/9-websocket/src/server.mjs
@@ -0,0 +1,24 @@
+import { createServer } from 'node:http'
+import { createReadStream } from 'node:fs'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { Server } from 'socket.io'
+const PORT = 5000
+const app = createServer((req, res) => {
+  const filename = resolve(dirname(fileURLToPath(import.meta.url)), 'index.html')
+  createReadStream(filename).pipe(res) 
+})
+
+app.listen(PORT, () => {
+  console.log('running http://localhost:'+PORT)
+})
+
+const io = new Server(app)
+
+io.on('connection', (socket) => {
+  console.log('nouvel utilisateur')
+  socket.emit('hello', 'Bienvenue')
+  socket.on('hi', (data) => {
+    console.log('message recu du client', data)
+  })
+})
